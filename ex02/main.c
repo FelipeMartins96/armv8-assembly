d428b2e4f6b557998aa814b303bd8dbceb402e5c
Felipe Martins <felipemartins96@gmail.com>
2020-08-15 13:43:32 -0300

    Exercise 02

--- ex02/main.c
+++ ex02/main.c
@@ -7,14 +7,12 @@ extern void assembly(long long int* a, long long int* b, long long int* c, long 
 int main(void)
 {
     long long int a, b, c, x;
-    while(1){
-		printf("a b c x -> ");
-		scanf("%lld %lld %lld %lld", &a, &b, &c, &x);
+	printf("a b c x -> ");
+	scanf("%lld %lld %lld %lld", &a, &b, &c, &x);
 
-		assembly(&a, &b, &c, &x);
+	assembly(&a, &b, &c, &x);
 
-		printf("x = %lld\n", x);
-    }
+	printf("x = %lld\n", x);
 
     return 0;
 }
