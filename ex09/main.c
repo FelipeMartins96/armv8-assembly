3b57c421f7af216acb694df57a99750a7fd703cb
Felipe Martins <felipemartins96@gmail.com>
2020-08-18 22:10:10 -0300

    Exercise 09

--- ex09/main.c
+++ ex09/main.c
@@ -6,11 +6,13 @@ extern long long int assembly(char* string, char* word);
 
 int main(void)
 {
-	char str[] = "grnrclszemskvbgcluwtgyvieip";
-	char word[] = "leg";
+	char str[] = "aaaaa";
+	char word[] = "aa";
 	long long int result;
 
 	result = assembly(str, word);
+
 	printf("%lld \n", result);
+
     return 0;
 }
